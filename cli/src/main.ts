7dbc5d30b55f1b4bece931c2d8d41ef99b897d52
Quentin <contact@yovach.dev>
2025-09-07 16:47:13 +0200

    feat: move @tsconfig/* dependencies to devDependencies, check if folder exists and add colors

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -1,17 +1,24 @@
+import { existsSync } from "node:fs";
+import { join as pathJoin } from "node:path";
+import { styleText } from "node:util";
 import prompts from "prompts";
+
 const response = await prompts([
   {
     type: "text",
     name: "directory",
     message: "In which folder do you want to create your project?",
-    validate: (text) => text.length >= 1,
+    validate: (text) =>
+      text.length >= 1
+        ? true
+        : styleText(["red"], "Please enter at least 1 character"),
   },
   {
     type: "autocomplete",
     name: "value",
     choices: [
       {
-        title: "Node.js with TypeScript",
+        title: styleText(["bgBlue", "white"], " Node.js + TypeScript "),
         value: "node",
         description: "A Node.js project with TypeScript and Docker",
       },
@@ -20,9 +27,13 @@ const response = await prompts([
   },
 ]);
 
+if (existsSync(pathJoin(response.directory))) {
+  console.error("This folder already exists");
+  process.exit(1);
+}
+
 const degit = await import("degit");
 const emitter = degit.default(
   `yovach/create-docker-starter/templates/${response.value}`,
-  { cache: true },
 );
 await emitter.clone(response.directory);
