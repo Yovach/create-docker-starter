97ce4a413dabe19ad660bdd710f9e0c704cfe55e
Quentin <contact@yovach.dev>
2025-09-15 11:50:42 +0000

    feat: use tar dependency to extract tar.gz file and clone specified template folder

--- cli/src/degit.ts
+++ cli/src/degit.ts
@@ -1,19 +1,39 @@
-import { createWriteStream, read } from "node:fs";
+import { existsSync } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import path from "node:path";
 import { Readable } from "node:stream";
+import { x } from "tar";
 
 export async function degit(
   repository: string,
+  repositoryFolder: string,
   folder: string,
-  branch: string = "main",
 ): Promise<void> {
-  const response = await fetch(repository);
+  const repositoryUrl = `${repository}/archive/main.tar.gz`;
+  const response = await fetch(repositoryUrl);
   if (!response.body) {
     throw new Error("Invalid body of response");
   }
 
-  console.log(response.body);
+  if (folder == null) {
+    throw new Error("Missing destination folder");
+  }
 
-  const writableStream = createWriteStream("./output/");
-  const readableStream = Readable.fromWeb(response.body);
-  readableStream.pipe(writableStream);
+  const destinationFolder = path.join(folder);
+  if (!existsSync(destinationFolder)) {
+    await mkdir(destinationFolder, { recursive: true });
+  }
+
+  Readable.fromWeb(response.body).pipe(
+    x({
+      cwd: destinationFolder,
+      strip: 3,
+      strict: true,
+      filter(path): boolean {
+        return path.startsWith(
+          "create-docker-starter-main/" + repositoryFolder,
+        );
+      },
+    }),
+  );
 }
